5417719f6c3c0c5cdec355bf680f2320b822dbe9
K4rakara <jack@insertdomain.name>
2020-08-06 15:38:38 -0400

    Add "advanced" functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-#[macro_use] extern crate lazy_static;
-extern crate regex;
+#[macro_use] pub(crate) extern crate lazy_static;
+#[cfg(feature = "advanced")] pub(crate) extern crate mlua;
+pub(crate) extern crate rand;
+pub(crate) extern crate regex;
 
-mod basic;
-mod replacements;
-mod emoji;
+#[cfg(feature = "advanced")] pub(crate) mod advanced;
+pub(crate) mod basic;
+pub(crate) mod emoji;
+pub(crate) mod replacements;
 
 pub use basic::{ clml };
+#[cfg(feature = "advanced")] pub use advanced::*;
